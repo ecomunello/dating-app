2655dbc5b0ce61f88835741f9b16751d7303286c
Enrico Comunello <comu1987@gmail.com>
2024-05-31 17:16:10 +0200

    remove error

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,9 +1,3 @@
 //https://nextjs.org/docs/app/building-your-application/routing/route-handlers
 
 
-export async function POST(request: Request) {
-    const formData = await request.formData()
-    const name = formData.get('name')
-    const email = formData.get('email')
-    return Response.json({ name, email })
-  }
